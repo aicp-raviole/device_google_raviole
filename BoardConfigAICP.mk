e2b5268b8c2ae7e9da27a8bb06ed3465f4ea7926
Alessandro Astone <ales.astone@gmail.com>
2021-03-30 11:34:38 +0200

    Build PowerShare HAL
    Change-Id: I3c0321b844acb5f97ac398acc842501410a44b6c
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- BoardConfigAICP.mk
+++ BoardConfigAICP.mk
@@ -14,6 +14,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     EuiccSupportPixelOverlay
 
+# PowerShare
+include hardware/google/pixel/powershare/device.mk
+
 # Touch
 include hardware/google/pixel/touch/device.mk
 
